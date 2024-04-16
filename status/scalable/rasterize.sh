16761529c0bf749ac79849372e54bd32462b6b8d
Manos Pitsidianakis <manos@pitsidianak.is>
2024-04-16 18:19:19 +0300

    Reduce antialiasing in rasterize.sh

--- status/scalable/rasterize.sh
+++ status/scalable/rasterize.sh
@@ -1,17 +1,20 @@
 #!zsh
 
-set v
-
 scale() {
   for dir in $(find .. -type d -name '[0-9]*'); do
     #echo scale dir is "$dir"
     export DIM=$(echo $dir | sed -e 's/[^0-9]*//g')
     #echo dim is $DIM
-    convert -background none "${1}" -resize ${DIM}x${DIM} "${dir}"/"$(basename "${1}" .svg)".png
+    convert -filter Lanczos -background none "${1}" -resize ${DIM}x${DIM} "${dir}"/"$(basename "${1}" .svg)".png
   done
 }
 
-for svg in $(git ls-files --modified '*.svg'); do
-  echo `realpath $svg`
-  scale `realpath $svg`
-done
+if [ -z "$1" ]; then
+  for svg in $(git ls-files --modified '*.svg'); do
+    echo `realpath $svg`
+    scale `realpath $svg`
+  done
+else
+    echo `realpath "${1}"`
+    scale `realpath "${1}"`
+fi
