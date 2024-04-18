0c1ca1882f640a2f59719a0836eab1f5bae440bf
Manos Pitsidianakis <manos@pitsidianak.is>
2024-04-18 10:08:54 +0300

    Change folder icon dithering background
    Signed-off-by: Manos Pitsidianakis <manos@pitsidianak.is>

--- status/scalable/rasterize.sh
+++ status/scalable/rasterize.sh
@@ -5,16 +5,33 @@ scale() {
     #echo scale dir is "$dir"
     export DIM=$(echo $dir | sed -e 's/[^0-9]*//g')
     #echo dim is $DIM
-    convert -filter Lanczos -background none "${1}" -resize ${DIM}x${DIM} "${dir}"/"$(basename "${1}" .svg)".png
+    export OUTPUT="${dir}"/"$(basename "${1}" .svg)".png
+    if [ -L "${OUTPUT}" ]; then
+      printf "Skipping symbolic link %s\n" "${OUTPUT}"
+      continue
+    fi
+    rm -f "${OUTPUT}"
+    convert -filter Lanczos -background none "${1}" -resize ${DIM}x${DIM} "${OUTPUT}"
+  done
+}
+
+rasterize_svg() {
+  printf "Converting %s\n" "${1}"
+  echo `realpath $1`
+  scale `realpath $1`
+
+  for link in $(find -L ../.. -samefile ${1} -xtype l); do
+    printf "Converting symbolic link %s\n" "${link}"
+    echo `realpath --no-symlinks $link`
+    scale `realpath --no-symlinks $link`
   done
 }
 
 if [ -z "$1" ]; then
   for svg in $(git ls-files --modified '*.svg'); do
-    echo `realpath $svg`
-    scale `realpath $svg`
+    rasterize_svg "${svg}"
   done
 else
-    echo `realpath "${1}"`
-    scale `realpath "${1}"`
+    echo `realpath --no-symlinks "${1}"`
+    scale `realpath --no-symlinks "${1}"`
 fi
